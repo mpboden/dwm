ab011585fd40db4ac074dbedcee86b06c892e449
Mike Boden <mike@mikeboden.com>
2024-03-28 22:45:22 -0700

    Configure Thuderbird on tag 7

--- config.h
+++ config.h
@@ -36,10 +36,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 6,       1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class         instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",        NULL,       NULL,       1 << 6,       1,           -1 },
+	{ "Thunderbird", NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "thunderbird", NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Firefox",     NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "firefox",     NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
